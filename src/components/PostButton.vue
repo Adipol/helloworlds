611015b40bb050d7cc29a2fb53474715fd387e71
pc-work <adipol13@gmail.com>
2023-03-30 17:30:55 -0400

    2 commit

---
+++ src/components/PostButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="post-btn">ACCEDER</button>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+.post-btn{
+    padding: 15px;
+    background-color: #892be2;
+    color: white;
+    border: 0;
+    outline: 0;
+}
+
+.post-botton:hover{
+    background-color: white;
+    color: blueviolet;
+}
+</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="post-card">
+        <h3>titulo</h3>
+        <p>Et dolor eos lorem consetetur ipsum et sit aliquyam. Ipsum eos sit nonumy gubergren eirmod eirmod. Aliquyam ipsum et ut.</p>
+        <PostButton/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import PostButton from './PostButton.vue';
+</script>
+
+<style scoped>
+.post-card {
+    border-radius: 15px;
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid #ccc;
+}
+</style>
