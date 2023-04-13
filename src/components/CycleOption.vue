0785e5d1316f18031d12b651cd8814261bdeb2ec
pc-work <adipol13@gmail.com>
2023-04-13 11:53:48 -0400

    ciclo de vida en composition api

---
+++ src/components/CycleOption.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hola mundo</div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, onUpdated } from "vue";
+onMounted(() => {
+  console.log("Mounted");
+});
+onUpdated(() => {
+  console.log("Updated");
+});
+</script>
+
+<style scoped></style>
